cac0f3af4d6b2e8f1bd7093bc1099ec387b05e79
Vladimir Kotal <vlada@devnull.cz>
2021-09-14 22:18:18 +0200

    adjust comment to say exit value

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ import logging
 
 
 def check_dir(name):
-    """Check if name is directory. If not, exit the program."""
+    """
+    Check if name is directory. If not, exit the program with error code 1.
+    """
 
     logger = logging.getLogger(__name__)
 
