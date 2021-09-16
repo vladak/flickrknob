3f8087a53eaeb6746d880d4ea004ff867cae615d
Vladimir Kotal <vlada@devnull.cz>
2021-09-16 22:29:14 +0200

    add author

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 
 setup(name='flickrknob',
       version='0.1',
+      author='Vladimir Kotal',
+      author_email='vlada@devnull.cz',
       # list folders, not files
       packages=['flickrknob'],
       scripts=['flickrknob/bin/flickrUploader.py'],
