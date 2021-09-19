e3bf28c9426cec0fdd0184756d0359a89c782bae
Vladimir Kotal <vlada@devnull.cz>
2021-09-19 22:41:49 +0200

    remove TODO

--- flickrknob/uploader.py
+++ flickrknob/uploader.py
@@ -79,14 +79,13 @@ def uploader():
         logger.error("Duplicate album name: '{}'".format(args.photoset))
         sys.exit(1)
 
-    # Upload photos in the top level of the directory.
+    # Upload files in the top level of the directory.
     dir_name = args.sourceDir
     logger.info("Getting list of files from '{}'".format(dir_name))
     dir_entries = [os.path.join(dir_name, f) for f in os.listdir(dir_name)
                    if os.path.isfile(os.path.join(dir_name, f))
                    and is_known_suffix(f)]
 
-    # TODO: check what happens if file cannot be read or lacks EXIF data
     logger.info("Sorting files")
     try:
         dir_entries.sort(key=get_exif_date)
