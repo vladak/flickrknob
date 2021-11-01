9eeede07d0c45e445ead2e4b3b53e5337d32d55a
Vladimir Kotal <vlada@devnull.cz>
2021-11-01 17:57:40 +0100

    fallback to last modified date on lack of EXIF
    fixes #8

--- flickrknob/photoutils.py
+++ flickrknob/photoutils.py
@@ -5,6 +5,7 @@ Utility functions for working with photo files.
 """
 
 import logging
+import os.path
 from datetime import datetime
 
 import exifread
@@ -30,31 +31,42 @@ def get_exif_date(file_path):
     logger.debug(f"Getting EXIF tags for '{file_path}'")
 
     tag_name = "DateTimeOriginal"
-    try:
-        with open(file_path, "rb") as fobj:
-            tags = exifread.process_file(fobj, stop_tag=tag_name)
-            try:
-                date_original = tags["EXIF " + tag_name]
-            except KeyError:
-                # pylint: disable=W0707
-                raise EXIFerror(f"File '{file_path}' lacks EXIF " "{tag_name} tag")
 
-            if date_original:
-                try:
-                    date_obj = datetime.strptime(
-                        str(date_original), "%Y:%m:%d %H:%M:%S"
-                    )
-                    return date_obj
-                except ValueError:
-                    # pylint: disable=W0707
-                    raise EXIFerror(
-                        f"{tag_name} tag not correctly formed " "for '{file_path}'"
-                    )
-    except PermissionError as exc:
-        # pylint: disable=W0707
-        raise EXIFerror(f"Permission problem for '{file_path}': {exc}")
+    with open(file_path, "rb") as fobj:
+        tags = exifread.process_file(fobj, stop_tag=tag_name)
+        try:
+            date_original = tags["EXIF " + tag_name]
+        except KeyError:
+            # pylint: disable=W0707
+            raise EXIFerror(f"File '{file_path}' lacks EXIF " "{tag_name} tag")
+
+        if date_original:
+            try:
+                date_obj = datetime.strptime(
+                    str(date_original), "%Y:%m:%d %H:%M:%S"
+                )
+                return date_obj
+            except ValueError:
+                # pylint: disable=W0707
+                raise EXIFerror(
+                    f"{tag_name} tag not correctly formed " "for '{file_path}'"
+                )
 
     raise EXIFerror(f"cannot find {tag_name} in '{file_path}'")
+
+
+def get_date(file_path):
+    """
+    Return date for given file. Will try extracting the date from the EXIF data first.
+    If not successful, fall back to the last modified date.
+    """
+    logger = logging.getLogger(__name__)
+
+    try:
+        return get_exif_date(file_path)
+    except EXIFerror as exc:
+        logger.debug(f"could not get EXIF date for {file_path}: {exc}")
+        return datetime.fromtimestamp(os.path.getmtime(file_path))
 
 
 def is_known_suffix(file_name):
